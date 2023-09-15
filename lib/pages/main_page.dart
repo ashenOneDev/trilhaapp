d8f508d46d8eed88a34210478b6dbac34f76d446
ashenOneDev <iagocav@gmail.com>
2023-09-15 10:18:38 -0300

    main page com drawer

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -10,6 +10,54 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Main Page"),
+        ),
+        drawer: Drawer(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                  child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 0, vertical: 5),
+                      width: double.infinity,
+                      child: const Text("Meus Dados")),
+                  onTap: () {},
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Divider(),
+                InkWell(
+                  child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 0, vertical: 5),
+                      width: double.infinity,
+                      child: const Text("Configurações")),
+                  onTap: () {},
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Divider(),
+                InkWell(
+                  child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 0, vertical: 5),
+                      width: double.infinity,
+                      child: const Text("Termos de uso e privacidade")),
+                  onTap: () {},
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
